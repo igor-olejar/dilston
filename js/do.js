b9db0ca5e5a4d6775053cd637a2a7479a4e4e9c9
Igor Olejar <igor.olejar@gmail.com>
2015-06-18 13:31:13 +0100

    jquery and basic structure with sliders

--- js/do.js
+++ js/do.js
@@ -1,3 +1,22 @@
 // setup
-var dilston = new Dilston();
-dilston.findPortNumber();
+//var dilston = new Dilston();
+//dilston.findPortNumber();
+
+// jQuery
+$(function() {
+    $(".cc-slider").slider({
+        min: 0,
+        max: 127,
+        value: 0,
+        change: function(event, ui) {
+            var cc_number = $(this).data('ccnumber');
+            console.log(ui.value);
+        }
+        /*slide: function(event, ui) {
+            var cc_number = $(this).data('ccnumber');
+            console.log(ui.value);
+        }*/
+    });
+
+
+});
